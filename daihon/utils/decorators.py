f98996eaf0d7db1e30903d6e065025560cc0407e
gimi65536 <gimi65536@hotmail.com>
2020-06-22 20:36:27 +0800

    改變callbackmethod的儲存方式&簡化class 與其把callback存在obj裡頭 既然都把obj各自對應的function obj做出來了 那就直接利用這個obj就行了，如同一般函數那般 如此一來callbackfunction與callbackmethod對應的class也能抽出base class

--- daihon/utils/decorators.py
+++ daihon/utils/decorators.py
@@ -1,20 +1,28 @@
 from functools import wraps
 
-#store info in function
-class CallbackFunction:
+class BaseCallback:
 	def __init__(self, f):
 		self._f = f
 		self._callbacks = []
 	def __call__(self, *args, **kwargs):
-		result = self._f(*args, **kwargs) #don't catch exceptions
+		result = self._call_part(*args, **kwargs) #don't catch exceptions
 		#if no exception accured, callback
+		self._callback_part(result)
+		return result
+	def _call_part(self, *args, **kwargs):
+		raise NotImplementedError
+	def _callback_part(self, result):
 		for callback in self._callbacks:
 			callback(result)
-		return result
 	def register(self, callback):
 		self._callbacks.append(callback)
 
-#store info in obj
+#store info in function
+class CallbackFunction(BaseCallback):
+	def _call_part(self, *args, **kwargs):
+		return self._f(*args, **kwargs)
+
+#make every obj.f an isolated function
 class CallbackMethodWrapper:
 	def __init__(self, f):
 		self._f = f
@@ -24,37 +32,24 @@ class CallbackMethodWrapper:
 			#return self._f
 			obj = klass
 
-		info = f'_callback{id(self)}'
-		#here not use hasattr
-		#to ensure the callbacks of klass do not interfere the one of the obj
-		if info not in obj.__dict__:
-			setattr(obj, info, [])
-
 		if id(obj) not in self._cache:
-			self._make_func(obj, info)
+			self._make_func(obj)
 		#it is impossible to make other obj have same ID
 		#because CallbackMethod stores the obj
 		#which makes the obj won't be deleted by GC
 		#and that makes the other object won't occupy the same ID
 
 		return self._cache[id(obj)][1]
-	def _make_func(self, obj, info):
-		result_func = wraps(self._f)(CallbackMethod(self._f, obj, info))
+	def _make_func(self, obj):
+		result_func = wraps(self._f)(CallbackMethod(self._f, obj))
 		self._cache[id(obj)] = (obj, result_func)
 
-class CallbackMethod:
-	def __init__(self, f, obj, info):
-		self._f = f
+class CallbackMethod(BaseCallback):
+	def __init__(self, f, obj):
+		super().__init__(f)
 		self._obj = obj
-		self._info = info
-	def __call__(self, *args, **kwargs):
-		result = self._f(self._obj, *args, **kwargs) #don't catch exceptions
-		#if no exception accured, callback
-		for callback in getattr(self._obj, self._info):
-			callback(result)
-		return result
-	def register(self, callback):
-		getattr(self._obj, self._info).append(callback)
+	def _call_part(self, *args, **kwargs):
+		return self._f(self._obj, *args, **kwargs)
 
 def callbackfunction(f):
 	return wraps(f)(CallbackFunction(f))
